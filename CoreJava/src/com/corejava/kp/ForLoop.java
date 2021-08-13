e92f20d63a7643f7f4d930922450d5b2016be11b
khpawar <khushi02pawar@gmail.com>
2021-08-13 09:30:56 +0530

    code added for loop

---
+++ CoreJava/src/com/corejava/kp/ForLoop.java
@@ -0,0 +1,11 @@
+package com.corejava.kp;
+
+public class ForLoop {
+	public static void main(String args []){
+		for(int x = 10; x <= 15; x++ ){
+			System.out.println("value of x : " + x);
+			System.out.println("\n");
+		}
+	}
+
+}
