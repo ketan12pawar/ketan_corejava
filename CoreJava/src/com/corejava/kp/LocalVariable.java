8d026d4eb031c14887dda9564c9457b5cfab481c
khpawar <khushi02pawar@gmail.com>
2021-08-13 16:53:09 +0530

    Create LocalVariable.java

---
+++ CoreJava/src/com/corejava/kp/LocalVariable.java
@@ -0,0 +1,15 @@
+package com.corejava.kp;
+
+      public class LocalVariable {
+    	  // a,b and c are local variable  to the main method.
+    	  
+    	  public static void main(String args []){
+    		  int a = 10;
+    		  int b = 20;
+    		  int c = a+b;
+    		  
+    		    System.out.println("sum 0f a and b is : "+c);
+    	  }
+
+}
+      //variable defined under any method is local to that particular method
