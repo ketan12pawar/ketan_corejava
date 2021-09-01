a107eb41aeff5e10d695426bbdb883691bc32584
khpawar <khushi02pawar@gmail.com>
2021-09-01 19:28:12 +0530

    code for ElapsedTime
    code for

---
+++ CoreJava/src/com/corejava/kp/DateDemo.java
@@ -0,0 +1,16 @@
+package com.corejava.kp;
+
+import java.util.Date;
+
+public class DateDemo {
+	
+	public static void main(String[] args){
+		//Instantiate a date object
+		Date dt = new Date();
+		
+		//display time and date using toString()
+		System.out.println(dt);
+		
+	}
+
+}
