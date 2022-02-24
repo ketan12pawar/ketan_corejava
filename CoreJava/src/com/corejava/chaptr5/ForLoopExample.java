976e7e5faf8ef77f9e9ad08a43cdbbb572b23fcb
khpawar <khushi02pawar@gmail.com>
2022-02-24 21:08:45 +0530

    code for chapter 5

---
+++ CoreJava/src/com/corejava/chaptr5/ForLoopExample.java
@@ -0,0 +1,11 @@
+package com.corejava.chaptr5;
+
+public class ForLoopExample {
+	public static void main(String[] args){
+		for(int x = 10; x<= 15; x++){
+			System.out.println("value of x : " + x);
+			System.out.println("\n");
+		}
+	}
+
+}
