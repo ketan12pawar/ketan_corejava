50b132479e96dd1e0b7fd189b253864c4ad8de45
khpawar <khushi02pawar@gmail.com>
2022-02-24 19:10:21 +0530

    code for chapter5

---
+++ CoreJava/src/com/corejava/chaptr5/IfElseIfExample.java
@@ -0,0 +1,44 @@
+package com.corejava.chaptr5;
+
+public class IfElseIfExample {
+	// This will be t9he staring point of the program 
+	public static void main(String[] args) {
+		// Declaring variable
+		int marks=65;
+		// Applying condition 1
+		
+		if(marks<50){
+			System.out.println("fail");
+		}
+		// Applying condition 2
+		
+		else if(marks>=50&& marks<60){
+			System.out.println("D grade");
+		}
+		// Applying condition 3
+		
+		else if(marks>=60&& marks<70){
+			System.out.println("C grade");
+		}
+		// Applying condition 4
+		else if(marks>=70&& marks<80){
+			System.out.println("B grade");
+		}
+		// Applying condition 5
+		else if(marks>=80&& marks<90){
+			System.out.println("A grade");
+		} 
+		// Applying condition 6
+		else if(marks>=90&& marks<100){
+			System.out.println("A+ grade");
+				
+		}
+		// Applying condition 7
+		
+		else {
+			System.out.println("Invalid");
+		}
+		
+	}
+
+}
