23350598486b6f1e0cd3b9e3159dc32c0177ee97
khpawar <khushi02pawar@gmail.com>
2022-02-24 21:34:19 +0530

    code for chapter2

--- CoreJava/src/com/corejava/chaptr2/Maths.java
+++ CoreJava/src/com/corejava/chaptr2/Maths.java
@@ -10,20 +10,22 @@ public class Maths{
 		// creating objcet
 		Maths obj1 = new Maths();
 		
-		// calling the method 
+		// calling the non static method
+		// with object
 		int l = obj1.sumOfNumber(50, 40);
 		
 		System.out.println("*******Sum of number*******" + l);
-		
-	/*	 int p = obj1.multiplicationOfNumber(50, 10);
-		 System.out.println("*******Sum of number*******" + p);*/
-		
+
+		//calling static method with just class name directly
+		//object creation not needed.
 		int p = Maths.multiplicationOfNumber(50,30);
+		
 		System.out.println("*******Sum of number*******" + p);
 	}
 	
 	
-	
+	// this is non static method
+	// this would require a class object to call this
 	public int sumOfNumber(int x, int y){
 		
 		int sum = x + y;
@@ -31,7 +33,7 @@ public class Maths{
 		return sum;
 		
 	}
-	
+	// whenever we want to call a method directly by class name we use static keyword
 	 public static int multiplicationOfNumber (int x ,int y){
 		 int multiplication = x * y;
 		 return multiplication;
